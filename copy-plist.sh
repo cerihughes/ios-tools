7ade78ea7a6630a3489f69fad77d3fb11a465fd2
Ceri Hughes <github@cerihughes.co.uk>
2020-10-01 15:37:11 +0100

    fix plist tool

--- copy-plist.sh
+++ copy-plist.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-current_directory="$(dirname "$0")"
-cd $current_directory/..
-
-cp $PROJECT_DIR/Source/Info.plist $PROJECT_DIR/Source/Secret.plist
-sed "s/_BUILD_NUMBER_/`git rev-list HEAD --count`/" $PROJECT_DIR/Source/Info.plist > $PROJECT_DIR/Source/Secret.plist
+cp "$PROJECT_DIR"/Source/Info.plist "$PROJECT_DIR"/Source/Secret.plist
+sed "s/_BUILD_NUMBER_/`git rev-list HEAD --count`/" "$PROJECT_DIR"/Source/Info.plist > "$PROJECT_DIR"/Source/Secret.plist
